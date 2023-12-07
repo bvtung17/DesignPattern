1228593e03cdaa3c7d409155f80834d430c83a5b
Van Tung <bvtung17@gmail.com>
2023-12-07 11:42:26 +0700

    Add project files.

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿// Interface định nghĩa các thuật toán
+public interface IStrategy
+{
+    void Execute();
+}
+
+// Các thuật toán cụ thể sẽ implement interface IStrategy
+public class ConcreteStrategyA : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Thực hiện thuật toán A");
+    }
+}
+
+public class ConcreteStrategyB : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Thực hiện thuật toán B");
+    }
+}
+
+// Context sử dụng một thuật toán thông qua interface IStrategy
+public class Context
+{
+    private IStrategy strategy;
+
+    // Thiết lập thuật toán ban đầu thông qua constructor hoặc phương thức setter
+    public Context(IStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+
+    // Cho phép client thay đổi thuật toán tại runtime
+    public void SetStrategy(IStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+
+    // Thực hiện thuật toán hiện tại
+    public void ExecuteStrategy()
+    {
+        strategy.Execute();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // Tạo context với một thuật toán ban đầu
+        Context context = new Context(new ConcreteStrategyA());
+
+        // Thực hiện thuật toán A
+        context.ExecuteStrategy();
+
+        // Thay đổi thuật toán thành B và thực hiện thuật toán B
+        context.SetStrategy(new ConcreteStrategyB());
+        context.ExecuteStrategy();
+    }
+}
---
+++ StrategyPatternDI/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public enum Operator
+{
+    Add,
+    Substract,
+    Multiple,
+    Divide
+}
+
+// Interface định nghĩa các thuật toán
+public interface IStrategy
+{
+    int Calculate(int a, int b, Operator op);
+}
+
+public interface IMathOperator
+{
+    Operator Operator { get; }
+    int Calculate(int a, int b);
+}
+
+
+// Các thuật toán cụ thể sẽ implement interface IStrategy
+public class AddOperator : IMathOperator
+{
+    public Operator Operator => Operator.Add;
+
+    public int Calculate(int a, int b) => a + b;
+}
+
+
+public class DivideOperator : IMathOperator
+{
+    public Operator Operator => Operator.Divide;
+
+    public int Calculate(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return a / b;
+    }
+}
+public class SubtractOperator : IMathOperator
+{
+    public Operator Operator => Operator.Substract;
+
+    public int Calculate(int a, int b) => a - b;
+}
+
+public class MultipleOperator : IMathOperator
+{
+    public Operator Operator => Operator.Multiple;
+
+    public int Calculate(int a, int b) => a * b;
+}
+
+// Service sử dụng một thuật toán thông qua interface IStrategy
+public class MathStrategy : IStrategy
+{
+    private readonly IEnumerable<IMathOperator> _operators;
+
+    public MathStrategy(IEnumerable<IMathOperator> operators) => _operators = operators;
+
+    public int Calculate(int a, int b, Operator op)
+        => _operators.FirstOrDefault(x => x.Operator == op)?.Calculate(a, b) ?? throw new ArgumentNullException(nameof(op));
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        // Khởi tạo DI container
+        var serviceProvider = new ServiceCollection()
+            .AddScoped<IStrategy, MathStrategy>()
+            .AddScoped<IMathOperator, AddOperator>()
+            .AddScoped<IMathOperator, SubtractOperator>()
+            .AddScoped<IMathOperator, MultipleOperator>()
+            .AddScoped<IMathOperator, DivideOperator>()
+            .BuildServiceProvider();
+
+        // Lấy một đối tượng Service từ DI container
+        var service = serviceProvider.GetRequiredService<IStrategy>();
+
+        // Thực hiện thuật toán thông qua Service
+        var value = service.Calculate(1, 2, Operator.Add);
+        Console.WriteLine(value);
+    }
+}
